8a3a869bb2711f051a3054a34f3607dc90ba52e6
Dharshini PraveenRaj <dharsh@Dharshinis-MacBook-Air.local>
2024-10-05 18:07:39 +0200

    my python works

---
+++ 2ndDemo.py
@@ -0,0 +1,15 @@
+
+# Trying new data type - List (List is a datatype that allows multiple values and can be with different datatypes)
+
+values =   [1,  2, "Dharshini", 4, 5]
+
+print(values[0])     # output should 1
+
+print(values[3])    #output should be 4
+
+print(values[-1])   # -1 prints the last value of the list , output should be 5
+
+print(values[1:3])  # ratio is used to print from and till ex : from 1 to 3rd value in the list
+
+values.insert(3,"PraveenRaj")
+print(values)
---
+++ FirstDemo.py
@@ -0,0 +1,27 @@
+from pprint import PrettyPrinter
+
+print("Hello")
+
+# Here are the comments that I have defined
+
+a = 3
+print(a)
+
+str =   "Hello world"
+print(str)
+
+b,  c,  d   =   1,  6.4,  "Great"
+print(b,c,d)
+
+# To print string and. integer together example below
+# To print the value of b
+
+print("{} {}".format("value is",b))
+
+# To know what datatype is used
+
+print(type(b))
+
+print(type(c))
+
+print(type(d))
